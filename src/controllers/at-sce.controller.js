172339ac6f61f295cf0c8115b4cf31dd8b83f0b4
JuanGalvez12 <jaun.galvez@agilethought.com>
2021-02-24 09:42:57 -0600

    add json

---
+++ src/controllers/at-sce.controller.js
@@ -0,0 +1,15 @@
+atSCEController.renderSigninForm = async () => {
+};
+
+atSCEController.signin = async () => {
+    
+};
+
+atSCEController.signout = async () => {
+    
+};
+  
+atSCEController.calculator = async () => {
+};
+
+module.exports = atSCEController;
