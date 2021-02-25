1598d4533f082aa8bcd6641f88455c1fd759a561
JuanGalvez12 <jaun.galvez@agilethought.com>
2021-02-25 13:27:46 -0600

    Copy all the POC's structure

--- src/controllers/at-sce.controller.js
+++ src/controllers/at-sce.controller.js
@@ -1,18 +1,45 @@
+/**
+ * AT SCE UI - AT SCE Controller.
+ * Copyright 2021 AgileThought, Inc.
+ *
+ * General functions for at-sce-controller.
+ *
+ * 
+ * @version 1.0
+ */
+
+ // AT SCE Controller
 const atSCEController = {};
 
-
+// AT-SCE - Index/Login
 atSCEController.renderSigninForm = async (req, res) => {
-};
-
-atSCEController.signin = async (req, res) => {
-    
-};
-
-atSCEController.signout = async (req, res) => {
-    
-};
+    console.log("--> atSCEController.renderSigninForm");
   
-atSCEController.calculator = async (req, res) => {
-};
-
-module.exports = atSCEController;
+    // Render
+    res.render("signin");
+  };
+  
+  atSCEController.signin = async (req, res) => {
+    console.log("--> atSCEController.signin");
+  
+    // Redirect
+    req.flash("success_msg", "User signin Successfully");
+    res.redirect("/calculator");
+  };
+  
+  // AT-SCE - Logout
+  atSCEController.signout = async (req, res) => {
+    console.log("--> atSCEController.signout");
+  
+    // Redirect
+    req.flash("success_msg", "User signout Successfully");
+    res.redirect("/signin");
+  };
+  
+  // AT-SCE - Calculator
+  atSCEController.calculator = async (req, res) => {
+    console.log("--> atSCEController.calculator");
+    res.render("calculator");
+  };
+  
+  module.exports = atSCEController;
---
+++ src/public/js/at-sce.util.js
@@ -0,0 +1,49 @@
+/* Section: Calc Form */
+
+// Section: Fixed Cost
+$(function () {
+    $('[data-toggle="tooltipFixedCost"]').tooltip();
+  });
+  
+  // Section: About Project
+  $(function () {
+    $('[data-toggle="tooltipAboutProject"]').tooltip();
+  });
+  
+  // Section Calc Cost day/hr
+  $(function () {
+    $('[data-toggle="tooltipCostPerDay"]').tooltip();
+  });
+  
+  $(function () {
+    $('[data-toggle="tooltipCostPerHr"]').tooltip();
+  });
+  
+  // Section: Calc Results
+  $(function () {
+    $('[data-toggle="tooltipCost"]').tooltip();
+  });
+  
+  $(function () {
+    $('[data-toggle="tooltipTax"]').tooltip();
+  });
+  
+  $(function () {
+    $('[data-toggle="tooltipCharge"]').tooltip();
+  });
+  
+  $(function () {
+    $('[data-toggle="tooltipRevenue"]').tooltip();
+  });
+  
+  // Section: Show History Button
+  $(function () {
+    $('[data-toggle="tooltipShowHistory"]').tooltip();
+  });
+  
+  /* Calculator Functions */
+  
+  /* Section: History DataTable */
+  $(document).ready(function () {
+    $("#historyTable").DataTable();
+  });
