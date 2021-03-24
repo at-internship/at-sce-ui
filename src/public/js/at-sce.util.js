5da5f0c4668fad5654cb5779120b6f28629c256c
DianaArceoSuarez <diana.arceo@agilethought.com>
2021-03-24 15:16:15 -0600

    AT-SCE-UI 16 We mplemented camelCase, validation for empry fields and initialization of Variables

--- src/public/js/at-sce.util.js
+++ src/public/js/at-sce.util.js
@@ -41,14 +41,14 @@ $(function () {
   $('[data-toggle="tooltipShowHistory"]').tooltip();
 });
 
+/*Constants percent's IVA and percent's ISR*/
+const percentIva = 0.16;
+const percentIsrRetention = 0.1;
+const percentIvaRetention = 0.1067;
+
 /* Calculator Functions */
-function SumFixedCost() {
-  var rent = document.getElementById("rent").value;
-  var transport = document.getElementById("transport").value;
-  var telephone = document.getElementById("telephone").value;
-  var feeding = document.getElementById("feeding").value;
-  var others = document.getElementById("others").value;
-  var totalFixedCost =
+function calculateTotalFixedCost(rent, transport, telephone, feeding, others) {
+  let totalFixedCost =
     parseFloat(rent) +
     parseFloat(transport) +
     parseFloat(telephone) +
@@ -57,65 +57,137 @@ function SumFixedCost() {
   return totalFixedCost;
 }
 
-function CostPerDay() {
-  var days = document.getElementById("days").value;
-  var costDay = SumFixedCost() / days;
-  return costDay;
+function calculateCostPerDay(days, totalFixedCost) {
+  return totalFixedCost / days;
 }
 
-function CostPerHour() {
-  var hours = document.getElementById("hours").value;
-  var costHour = CostPerDay() / hours;
-  return costHour;
+function calculateCostPerHour(hours, costPerDay) {
+  return costPerDay / hours;
 }
 
-function TaxesIVAandISR() {
-  var ProjectCost = document.getElementById("projectcost").value;
-  var IVA = parseFloat(ProjectCost) * 0.16;
-  var ISR_retention = parseFloat(ProjectCost) * 0.1;
-  var IVA_retention = parseFloat(ProjectCost) * 0.1067;
-
-  var TOTAL_taxes =
-    parseFloat(IVA) + parseFloat(ISR_retention) + parseFloat(IVA_retention);
-  return TOTAL_taxes;
+function calculatePercentIva(projectCost) {
+  return parseFloat(projectCost * percentIva);
 }
 
-function Charge() {
-  var ProjectCost = document.getElementById("projectcost").value;
+function calculatePercentIsrRetention(projectCost) {
+  return parseFloat(projectCost * percentIsrRetention);
+}
 
-  var IVA = parseFloat(ProjectCost) * 0.16;
-  var ISR_retention = parseFloat(ProjectCost) * 0.1;
-  var IVA_retention = parseFloat(ProjectCost) * 0.1067;
+function calculatePercentIvaRetention(projectCost) {
+  return parseFloat(projectCost * percentIvaRetention);
+}
 
-  var charge =
-    parseFloat(ProjectCost) +
-    parseFloat(IVA) -
-    parseFloat(ISR_retention) -
-    parseFloat(IVA_retention);
+function calculateTotalTaxesIVAandISR(
+  percentIva,
+  percentIsrRetention,
+  percentIvaRetention
+) {
+  let totalTaxes =
+    parseFloat(percentIva) +
+    parseFloat(percentIsrRetention) +
+    parseFloat(percentIvaRetention);
+  return totalTaxes;
+}
 
+function calculateCharge(
+  projectCost,
+  percentIva,
+  percentIsrRetention,
+  percentIvaRetention
+) {
+  let charge =
+    parseFloat(projectCost) +
+    parseFloat(percentIva) -
+    parseFloat(percentIsrRetention) -
+    parseFloat(percentIvaRetention);
   return charge;
 }
 
-function Revenue() {
-  var charge = Charge();
-  var totalFixedCost = SumFixedCost();
-  var revenue = parseFloat(charge) - parseFloat(totalFixedCost);
+function calculateRevenue(charge, totalFixedCost) {
+  let revenue = parseFloat(charge) - parseFloat(totalFixedCost);
   return revenue;
 }
 
-function Calculate() {
-  document.getElementById("ProjectWillCostYou").innerHTML =
-    "$" + parseFloat(SumFixedCost()).toFixed(2);
-  document.getElementById("CostPerDay").innerHTML =
-    "$" + parseFloat(CostPerDay()).toFixed(2);
-  document.getElementById("CostPerHour").innerHTML =
-    "$" + parseFloat(CostPerHour()).toFixed(2);
-  document.getElementById("TaxesIVAandISR").innerHTML =
-    "$" + parseFloat(TaxesIVAandISR()).toFixed(2);
-  document.getElementById("Charge").innerHTML =
-    "$" + parseFloat(Charge()).toFixed(2);
-  document.getElementById("Revenue").innerHTML =
-    "$" + parseFloat(Revenue()).toFixed(2);
+function calculateProjectCost() {
+  /*Initialization */
+  const rent = document.getElementById("rent").value;
+  const transport = document.getElementById("transport").value;
+  const telephone = document.getElementById("telephone").value;
+  const feeding = document.getElementById("feeding").value;
+  const others = document.getElementById("others").value;
+  const days = document.getElementById("days").value;
+  const hours = document.getElementById("hours").value;
+  const projectCost = document.getElementById("projectcost").value;
+
+  /*Validation*/
+  if (
+    rent == "" ||
+    transport == "" ||
+    telephone == "" ||
+    feeding == "" ||
+    others == "" ||
+    days == "" ||
+    hours == "" ||
+    projectCost == ""
+  ) {
+    document.getElementById("myAlert").innerHTML =
+      "<strong> Please, enter value in all fields </strong>";
+    document.getElementById("myAlert").className =
+      "alert alert-warning align-self-center mt-2";
+
+    $("#myAlert").fadeIn(1500);
+    setTimeout(function () {
+      $("#myAlert").fadeOut();
+    }, 5000);
+  } else {
+    /*Call the functions*/
+    let totalFixedCost = calculateTotalFixedCost(
+      rent,
+      transport,
+      telephone,
+      feeding,
+      others
+    );
+    let costPerDay = calculateCostPerDay(days, totalFixedCost);
+    let costPerHour = calculateCostPerHour(hours, costPerDay);
+    let percentIva = calculatePercentIva(projectCost);
+    let percentIsrRetention = calculatePercentIsrRetention(projectCost);
+    let percentIvaRetention = calculatePercentIvaRetention(projectCost);
+    let totalTaxesIVAandISR = calculateTotalTaxesIVAandISR(
+      percentIva,
+      percentIsrRetention,
+      percentIvaRetention
+    );
+    let charge = calculateCharge(
+      projectCost,
+      percentIva,
+      percentIsrRetention,
+      percentIvaRetention
+    );
+    let revenue = calculateRevenue(charge, totalFixedCost);
+    /* DOM handling */
+    document.getElementById("ProjectWillCostYou").innerHTML = parseFloat(
+      totalFixedCost
+    ).toFixed(2);
+    document.getElementById("CostPerDay").innerHTML = parseFloat(
+      costPerDay
+    ).toFixed(2);
+    document.getElementById("CostPerHour").innerHTML = parseFloat(
+      costPerHour
+    ).toFixed(2);
+    document.getElementById("TaxesIVAandISR").innerHTML = parseFloat(
+      totalTaxesIVAandISR
+    ).toFixed(2);
+    document.getElementById("Charge").innerHTML = parseFloat(charge).toFixed(2);
+    document.getElementById("Revenue").innerHTML = parseFloat(revenue).toFixed(
+      2
+    );
+    /*Highlight button*/
+    $(".calcResultsSection").effect("highlight", {}, 3000);
+
+    /*Enable Save Button */
+    $("#saveButton").prop("disabled", false);
+  }
 }
 
 /* Section: History DataTable */
