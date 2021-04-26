54a361057407d1794875aaf135610e22898b32bb
JuanGalvez12 <jaun.galvez@agilethought.com>
2021-04-26 09:27:05 -0500

    AT-SCE-UI 31  Refactor AT-SSO-UI

--- src/services/at-sce-api.service.js
+++ src/services/at-sce-api.service.js
@@ -27,6 +27,19 @@ AT_SCE_SERVICE.login = (data) => {
   });
 };
 
+//US 31 REFACTOR in branch feature/AT-SOO-UI_AdminUser_Refactor Login - POST /api/v1/login
+//New direction https://at-sso-api.herokuapp.com/api save on .env file like varible AT_SCE_SERVICE_URI
+/*AT_SCE_SERVICE.login = (data) => {
+  return axios({
+    method: "POST",
+    url: `${AT_SCE_SERVICE_URI}/v1/login`,
+    data: data,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+};*/
+
 // Operation: Get ALL USERS - GET/api/v1/users
 AT_SCE_SERVICE.getAllUsers = () => {
   return axios({
@@ -39,6 +52,20 @@ AT_SCE_SERVICE.getAllUsers = () => {
     console.log(`Error: ${error.message}`);
   });
 };
+
+//US 31 REFACTOR in branch feature/AT-SOO-UI_AdminUser_Refactor Get ALL USERS - GET/api/v1/users
+//New direction https://at-sso-api.herokuapp.com/api save on .env file like varible AT_SCE_SERVICE_URI
+/*AT_SCE_SERVICE.getAllUsers = () => {
+  return axios({
+    method: "GET",
+    url: `${AT_SCE_SERVICE_URI}/v1/users`,
+    headers: {
+      "content-type": "application/json",
+    },
+  }).catch(function (error) {
+    console.log(`Error: ${error.message}`);
+  });
+};*/
 
 // Operation: Get USER by ID - GET/api/v1/users/{id}
 AT_SCE_SERVICE.getUserById = (id) => {
@@ -53,6 +80,20 @@ AT_SCE_SERVICE.getUserById = (id) => {
   });
 };
 
+ //US 31 REFACTOR in branch feature/AT-SOO-UI_AdminUser_Refactor Get USER by ID - GET/api/v1/users/{id}
+ //New direction https://at-sso-api.herokuapp.com/api save on .env file like varible AT_SCE_SERVICE_URI
+/*AT_SCE_SERVICE.getUserById = (id) => {
+  return axios({
+    method: "GET",
+    url: `${AT_SCE_SERVICE_URI}/v1/users/${id}`,
+    headers: {
+      "content-type": "application/json",
+    },
+  }).catch(function (error) {
+    console.log(`Error: ${error.message}`);
+  });
+};*/
+
 // Operation; Create USER - POST /api/v1/users
 AT_SCE_SERVICE.createUser = (data) => {
   return axios({
@@ -64,6 +105,20 @@ AT_SCE_SERVICE.createUser = (data) => {
     },
   })
 };
+
+//US 31 REFACTOR in branch feature/AT-SOO-UI_AdminUser_Refactor Create USER - POST /api/v1/users
+//New direction https://at-sso-api.herokuapp.com/api save on .env file like varible AT_SCE_SERVICE_URI
+/*AT_SCE_SERVICE.createUser = (data) => {
+  return axios({
+    method: "POST",
+    url: `${AT_SCE_SERVICE_URI}/v1/users`,
+    data: data,
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+};
+*/
 
 // Operation: Update USER - PUT /api/v1/users
 AT_SCE_SERVICE.updateUser = (data) => {
@@ -77,6 +132,19 @@ AT_SCE_SERVICE.updateUser = (data) => {
   })
 };
 
+//US 31 REFACTOR in branch feature/AT-SOO-UI_AdminUser_Refactor Update USER - PUT /api/v1/users
+//New direction https://at-sso-api.herokuapp.com/api save on .env file like varible AT_SCE_SERVICE_URI
+/*AT_SCE_SERVICE.updateUser = (data) => {
+  return axios({
+    method: "PUT",
+    url: `${AT_SCE_SERVICE_URI}/v1/users/${data.id}`,
+    data: data,
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+};*/
+
 // Operation: Delete USER - DELETE /api/v1/users/{id}
 AT_SCE_SERVICE.deleteUser = (id) => {
   return axios({
@@ -87,6 +155,19 @@ AT_SCE_SERVICE.deleteUser = (id) => {
     },
   })
 };
+
+//US 31 REFACTOR in branch feature/AT-SOO-UI_AdminUser_Refactor Delete USER - DELETE /api/v1/users/{id}
+//New direction https://at-sso-api.herokuapp.com/api save on .env file like varible AT_SCE_SERVICE_URI
+/*AT_SCE_SERVICE.deleteUser = (id) => {
+  return axios({
+    method: "DELETE",
+    url: `${AT_SCE_SERVICE_URI}/v1/users/${id}`,
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+};
+*/
 
 // Operation: Get HISTORY by USER -  GET /api/v1/histories?userid={id}
 AT_SCE_SERVICE.getHistory = (id) => {
