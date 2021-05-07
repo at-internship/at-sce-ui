6137a887b170000f3ff0b30a2ad26cb9555bda05
DianaArceoSuarez <diana.arceo@agilethought.com>
2021-05-07 17:56:27 -0500

    AT-SCE-UI 27 We implemented the health check in the health-check.routes.js file

--- src/routes/health-check.routes.js
+++ src/routes/health-check.routes.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const router = express.Router();
+var packageJ = require("../../package.json");
+const { execSync } = require("child_process");
 
+function getGitCommitHash() {
+  return execSync("git rev-parse HEAD").toString().trim();
+}
+function getGitNameBranch() {
+  return execSync("git name-rev --name-only HEAD").toString().trim();
+}
 router.get("/", async (_req, res, _next) => {
   // optional: add further things to check (e.g. connecting to dababase)
   const healthcheck = {
+    version: packageJ.version,
     uptime: process.uptime(),
-    message: "OK",
+    message: "LIVE",
     timestamp: Date.now(),
-    branch: "feature/AT-SCE-UI_POC",
-    commit: "bd05d94",
+    branch: getGitNameBranch(),
+    commit: getGitCommitHash(),
+    flags: {
+      AT_SSO_SERVICE_URI_ENABLED: process.env.AT_SSO_SERVICE_URI_ENABLED,
+      AT_SSO_WEB_TOKEN_ENABLED: process.env.AT_SSO_WEB_TOKEN_ENABLED,
+      LOGIN_ENCRYPTION_ENABLED: process.env.LOGIN_ENCRYPTION_ENABLED,
+      CREATE_USER_ENCRYPTION_ENABLED: process.env.CREATE_USER_ENCRYPTION_ENABLED,
+      UPDATE_USER_ENCRYPTION_ENABLED: process.env.UPDATE_USER_ENCRYPTION_ENABLED,
+    },
+    services: {
+      AT_SCE_SERVICE_URI: process.env.AT_SCE_SERVICE_URI,
+      AT_SSO_SERVICE_URI: process.env.AT_SSO_SERVICE_URI,
+    },
   };
 
   try {
